d9310fa8e85b517a41942f5759001763a09aee3c
saiteja-git <saiteja.t1012@gmail.com>
2022-11-24 18:22:15 +0530

    Update new changes

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask_restful import Resource, reqparse
+import pandas as pd
+import json
+
+
+class Payload(Resource):
+
+    def __init__(self):
+        parser = reqparse.RequestParser()
+        self.metricname = parser.add_argument("metricname", type=str, required=True, 
+                                            help="This should be a platform")
+        self.content_partner = parser.add_argument("content_partner", action="append", type=str, required=False,
+                                                   help="This should be a content_partner")
+        self.device_model = parser.add_argument("device_model", action="append", type=str, required=False,
+                                                help="This should be a content_type")
+        self.device_platform = parser.add_argument("device_platform", action="append", type=str, required=False,
+                                              help="This should be a error_type")
+
+        self.content_type = parser.add_argument("content_type", action="append", type=str, required=False,
+                                            help="This should be a location")
+        self.cdn = parser.add_argument("cdn", action="append", type=str, required=False,
+                                            help="This should be a location")
+        self.location = parser.add_argument("location", action="append", type=str, required=False,
+                                            help="This should be a location")
+        self.aggregation_interval = parser.add_argument("aggregation_interval", action="append", type=str, required=False,
+                                            help="This should be a location")
+        self.data = parser.parse_args()
+        self.m_name = self.data.get("metricname", None)
+        self.con_partner = self.data.get("content_partner", None)
+        self.dev_model = self.data.get("device_model", None)
+        self.dev_plat = self.data.get("device_platform", None)
+        self.con_type = self.data.get("content_type", None)
+        self.cd_n = self.data.get("cdn", None)
+        self.lo_cation = self.data.get("location", None)
+        self.agg_interval = self.data.get("aggregation_interval", None)
+
+
+class Mulitiselection(Payload):
+
+    def __init__(self):
+        super().__init__()
+        self.m_name = self.m_name
+        self.con_partner = self.con_partner
+        self.dev_model = self.dev_model
+        self.dev_plat = self.dev_plat
+        self.con_type = self.con_type
+        self.cd_n = self.cd_n
+        self.lo_cation = self.lo_cation
+        self.agg_interval = self.agg_interval
+
+
+    def post(self):
+        return {"metricname": self.m_name,
+                "content_partner": self.con_partner,
+                "device_model": self.dev_model,
+                "device_platform":self.dev_plat,
+                "content_type": self.con_type,
+                "cdn":self.cd_n,
+                "location": self.lo_cation,
+                "aggregation_interval":self.agg_interval}
+
+
