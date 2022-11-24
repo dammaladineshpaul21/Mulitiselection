cd4bdb72545546e6b179d36917eb2acbc57ecff9
dammaldineshpaul21 <dammaladineshpaul21@gmail.com>
2022-11-25 02:05:52 +0530

    Updated new list of file

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 from flask_restful import Resource, reqparse
 import pandas as pd
 import json
+import boto3
+from boto3.dynamodb.conditions import key, Attr
+import os
+import requests
 
 
 class Payload(Resource):
 
     def __init__(self):
-        """This is a payload"""
+        """This is a payload""" 
         parser = reqparse.RequestParser()
         self.metricname = parser.add_argument("metricname", type=str, required=True, 
-                                            help="This should be a platform")
+                                                help="This should be a platform")
         self.content_partner = parser.add_argument("content_partner", action="append", type=str, required=False,
-                                                   help="This should be a content_partner")
+                                                help="This should be a content_partner")
         self.device_model = parser.add_argument("device_model", action="append", type=str, required=False,
                                                 help="This should be a content_type")
         self.device_platform = parser.add_argument("device_platform", action="append", type=str, required=False,
-                                              help="This should be a error_type")
+                                                help="This should be a error_type")
 
         self.content_type = parser.add_argument("content_type", action="append", type=str, required=False,
-                                            help="This should be a location")
+                                                help="This should be a location")
         self.cdn = parser.add_argument("cdn", action="append", type=str, required=False,
-                                            help="This should be a location")
+                                                help="This should be a location")
         self.location = parser.add_argument("location", action="append", type=str, required=False,
-                                            help="This should be a location")
+                                                help="This should be a location")
         self.aggregation_interval = parser.add_argument("aggregation_interval", action="append", type=str, required=False,
-                                            help="This should be a location")
+                                                help="This should be a location")
         self.data = parser.parse_args()
         self.m_name = self.data.get("metricname", None)
         self.con_partner = self.data.get("content_partner", None)
@@ -50,14 +54,5 @@ class Mulitiselection(Payload):
         self.agg_interval = self.agg_interval
 
 
-    def post(self):
-        return {"metricname": self.m_name,
-                "content_partner": self.con_partner,
-                "device_model": self.dev_model,
-                "device_platform":self.dev_plat,
-                "content_type": self.con_type,
-                "cdn":self.cd_n,
-                "location": self.lo_cation,
-                "aggregation_interval":self.agg_interval}
 
 
